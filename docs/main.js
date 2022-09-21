f73d6efc11e3c0878db58da4f00606f286aad3b0
Said Valencia Castrillo <50384031+said05@users.noreply.github.com>
2022-09-21 10:44:12 -0500

    adding docs folder

---
+++ docs/main.js
@@ -0,0 +1,180 @@
+const db = {
+  methods: {
+    find: (id) => {
+      return db.items.find((item) => item.id === id);
+    },
+    remove: (items) => {
+      items.forEach((item) => {
+        const product = db.methods.find(item.id);
+        product.qty = product.qty - item.qty;
+      });
+      console.log(db);
+    },
+  },
+  items: [
+    {
+      id: 0,
+      title: 'Funko Pop',
+      price: 250,
+      qty: 5,
+    },
+    {
+      id: 1,
+      title: 'Harry Potter',
+      price: 350,
+      qty: 50,
+    },
+    {
+      id: 2,
+      title: 'Phillips Hue',
+      price: 150,
+      qty: 40,
+    },
+    {
+      id: 3,
+      title: 'JavaScript Book',
+      price: 250,
+      qty: 10,
+    },
+  ],
+};
+
+const shoppingCart = {
+  items: [],
+  methods: {
+    add: (id, qty) => {
+      const cartItem = shoppingCart.methods.get(id);
+      console.log(cartItem);
+      if (cartItem) {
+        if (shoppingCart.methods.hasInventory(id, qty + cartItem.qty)) {
+          cartItem.qty += qty;
+        } else {
+          alert('There is not inventory for this product');
+        }
+      } else {
+        shoppingCart.items.push({ id, qty });
+      }
+    },
+    remove: (id, qty) => {
+      const cartItem = shoppingCart.methods.get(id);
+      if (cartItem.qty - qty > 0) {
+        cartItem.qty -= qty;
+      } else {
+        shoppingCart.items = shoppingCart.items.filter(
+          (item) => item.id !== id
+        );
+      }
+    },
+    count: () => {
+      return shoppingCart.items.reduce((acc, item) => acc + item.qty, 0);
+    },
+    get: (id) => {
+      const index = shoppingCart.items.findIndex((item) => item.id === id);
+      return index >= 0 ? shoppingCart.items[index] : null;
+    },
+    getTotal: () => {
+      const total = shoppingCart.items.reduce((acc, item) => {
+        const found = db.methods.find(item.id);
+        return acc + found.price * item.qty;
+      }, 0);
+      return total;
+    },
+    hasInventory: (id, qty) => {
+      return db.items.find((item) => item.id === id).qty - qty >= 0;
+    },
+    purchase: () => {
+      db.methods.remove(shoppingCart.items);
+      shoppingCart.items = [];
+    },
+  },
+};
+
+//rendering
+renderStore();
+
+function renderStore() {
+  const html = db.items.map((item) => {
+    return `<div class="item">
+    <div class="title">${item.title}</div>
+    <div class="price">${numberToCurrency(item.price)}</div>
+    <div class="qty">${item.qty} units</div>
+    <div class="actions">
+    <button class="add" data-id="${item.id}">Add to Shopping Cart</button>
+    </div>
+    </div>`;
+  });
+
+  document.querySelector('#store-container').innerHTML = html.join('');
+
+  document.querySelectorAll('.item .actions .add').forEach((button) => {
+    button.addEventListener('click', (e) => {
+      console.log('working');
+      const id = parseInt(button.getAttribute('data-id'));
+      const item = db.methods.find(id);
+      if (item && item.qty - 1 > 0) {
+        //adding to shopping cart
+        shoppingCart.methods.add(id, 1);
+        renderShoppingCart();
+      } else {
+        console.log('There is not inventory for this product');
+      }
+    });
+  });
+}
+
+function numberToCurrency(n) {
+  return new Intl.NumberFormat('en-US', {
+    maximumSignificantDigits: 2,
+    style: 'currency',
+    currency: 'USD',
+  }).format(n);
+}
+
+function renderShoppingCart() {
+  const html = shoppingCart.items.map((item) => {
+    const dbItem = db.methods.find(item.id);
+    return `<div class="item">
+    <div class="title">${dbItem.title}</div>
+    <div class="price">${numberToCurrency(dbItem.price)}</div>
+    <div class="qty">${item.qty} units</div>
+    <div class="subtotal">
+    Subtotal: ${numberToCurrency(item.qty * dbItem.price)}
+    </div>
+    <div class="actions">
+    <button class="add-one" data-id="${item.id}">+</button>
+    <button class="remove-one" data-id="${item.id}">-</button>
+    </div>
+    </div>`;
+  });
+
+  const closeButton = `<div class="cart-header">
+  <button class="button button-close">Close</button>
+  </div>`;
+  const purchaseButton =
+    shoppingCart.items.length > 0
+      ? `<div class="cart-actions">
+  <button class="button button-purchase">Purchase</button>
+  </div>`
+      : '';
+  const total = shoppingCart.methods.getTotal();
+  const totalContainer = `<div class="total>Total : ${numberToCurrency(
+    total
+  )}</div>`;
+  const shoppingCartContainer = document.querySelector(
+    '#shopping-cart-container'
+  );
+  shoppingCartContainer.innerHTML =
+    closeButton + html.join('') + totalContainer + purchaseButton;
+  document.querySelectorAll('.add-one').forEach((button) => {
+    button.addEventListener('click', (event) => {});
+  });
+  document.querySelectorAll('.remove-one').forEach((button) => {
+    button.addEventListener('click', (event) => {});
+  });
+  document
+    .querySelector('.button .button-close')
+    .addEventListener('click', (event) => {});
+  docuement
+    .querySelector('.button .button-purchase')
+    .addEventListener('click', (event) => {});
+}
